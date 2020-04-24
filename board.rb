d414c30a21434f6edb83b0d0ae67e3b5d43f9da2
Arthur Yao <yakunyao@gmail.com>
2020-04-24 09:30:14 +0800

    Add Board class with basic helper method to read or write baord

--- board.rb
+++ board.rb
@@ -1,6 +1,39 @@
 require_relative 'tile.rb'
 
 class Board
+    # default board size 9 * 9, with 10 random mines
 
+    def initialize
+        @board=Array.new(9) {Array.new(9)}
+        
+    end
+
+    def count_mines
+        board.flatten.count {|boolean| boolean}
+    end
+
+    def populate_mines
+        row = random(9)
+        col = random(9)
+
+    end
+
+    def [](position)
+        row, col = position
+        board[row][col]
+    end
+
+    def []=(position, value)
+        row, col = position
+        board[row][col] = value
+    end
+
+    def render
+
+    end
+
+    private
+
+    attr_accessor :board
 
 end
